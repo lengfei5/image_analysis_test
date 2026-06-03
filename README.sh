13b6d30c8670c60587679f26540a6d807f7ab425
jiwang <jingkui.wang@imp.ac.at>
2026-06-03 10:03:48 +0200

    add README.sh

---
+++ README.sh
@@ -0,0 +1,5 @@
+#########################################
+## Note for Deniz video tracking in python
+#########################################
+## A34_8wpt_2_D1 the circle is too large, which has lots of pixels brighter than the axolotl
+## some videos from A34_preOn_1, 4, 5, 6 with too low threshold
