f120d7a22abfc146dada03ea42cbb608e696e22f
jiwang <jingkui.wang@imp.ac.at>
2026-06-23 11:55:28 +0200

    save the sleap converting

---
+++ scripts/run_sleap.sh
@@ -0,0 +1,2 @@
+## cmd to convert sleap outcome to mp4 with specific marker_size and color
+sleap-render labels_20manual_trained_infer80Frame_manual80Correction_trainedModel_test_v1.v010.slp.nwb.slp  -o out_test4.mp4 -f 30 --marker_size 3 --palette solarized
