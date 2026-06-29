21449f1900c2bac61619286d3f25c5107daba5e1
jiwang <jingkui.wang@imp.ac.at>
2026-06-29 09:05:25 +0200

    save the readme

--- scripts/run_sleap.sh
+++ scripts/run_sleap.sh
@@ -1,2 +1,5 @@
 ## cmd to convert sleap outcome to mp4 with specific marker_size and color
 sleap-render labels_20manual_trained_infer80Frame_manual80Correction_trainedModel_test_v1.v010.slp.nwb.slp  -o out_test4.mp4 -f 30 --marker_size 3 --palette solarized
+
+sleap-render labels_153labels_upbodyCenter_added_predictionCleaned_trainedModel_170labels.v020.slp -f 30 --marker_size 3 --palette solarized -o out_multiAnimal_11keypoints_1.mp4
+
